f708ead2673fbb9484d8201c664ee97e63cd26c1
Andrew Rossignol <aarossig@google.com>
2017-02-27 13:08:49 -0800

    Adds the ability to build a binary to the build template
    Change-Id: I7c94f6f171a2b6e7e9f316264a5a060c6c13b1fe

--- build/build_template.mk
+++ build/build_template.mk
@@ -21,14 +21,18 @@ include $(CHRE_PREFIX)/build/defs.mk
 #                  shared object targets.
 #
 # Argument List:
-#     $1 - TARGET_NAME         - The name of the target being built.
-#     $2 - TARGET_CFLAGS       - The compiler flags to use for this target.
-#     $3 - TARGET_CC           - The C/C++ compiler for the target variant.
-#     $4 - TARGET_SO_LDFLAGS   - The linker flags to use for this target.
-#     $5 - TARGET_LD           - The linker for the target variant.
-#     $6 - TARGET_ARFLAGS      - The archival flags to use for this target.
-#     $7 - TARGET_AR           - The archival tool for the targer variant.
-#     $8 - TARGET_VARIANT_SRCS - Source files specific to this variant.
+#     $1  - TARGET_NAME         - The name of the target being built.
+#     $2  - TARGET_CFLAGS       - The compiler flags to use for this target.
+#     $3  - TARGET_CC           - The C/C++ compiler for the target variant.
+#     $4  - TARGET_SO_LDFLAGS   - The linker flags to use for this target.
+#     $5  - TARGET_LD           - The linker for the target variant.
+#     $6  - TARGET_ARFLAGS      - The archival flags to use for this target.
+#     $7  - TARGET_AR           - The archival tool for the targer variant.
+#     $8  - TARGET_VARIANT_SRCS - Source files specific to this variant.
+#     $9  - TARGET_BUILD_BIN    - Build a binary. Typically this means that the
+#                                 source files provided include an entry point.
+#     $10 - TARGET_BIN_LDFLAGS  - Linker flags that are passed to the linker
+#                                 when building an executable binary.
 #
 ################################################################################
 
@@ -59,6 +63,9 @@ $$(1)_SO = $(OUT)/$$$(1)/$(OUTPUT_NAME).so
 # Static Archive
 $$(1)_AR = $(OUT)/$$$(1)/$(OUTPUT_NAME).a
 
+# Optional Binary
+$$(1)_BIN = $$(if $(9), $(OUT)/$$$(1)/$(OUTPUT_NAME), )
+
 # Top-level Build Rule #########################################################
 
 # Define the phony target.
@@ -68,8 +75,11 @@ $(1)_ar: $$($$(1)_AR)
 .PHONY: $(1)_so
 $(1)_so: $$($$(1)_SO)
 
+.PHONY: $(1)_bin
+$(1)_bin: $$($$(1)_BIN)
+
 .PHONY: $(1)
-$(1): $(1)_ar $(1)_so
+$(1): $(1)_ar $(1)_so $(1)_bin
 
 # If building the runtime, simply add the archive and shared object to the all
 # target. When building CHRE, it is expected that this runtime just be linked
@@ -105,6 +115,10 @@ $$($$(1)_AR): $$(OUT)/$$$(1) $$($$$(1)_DIRS) $$($$(1)_CXX_OBJS) $$($$(1)_C_OBJS)
 $$($$(1)_SO): $$(OUT)/$$$(1) $$($$$(1)_DIRS) $$($$(1)_CXX_OBJS) $$($$(1)_C_OBJS)
 	$(5) $(4) -o $$@ $$(filter %.o, $$^)
 
+$$($$(1)_BIN): $$(OUT)/$$$(1) $$($$$(1)_DIRS) $$($$(1)_CXX_OBJS) \
+               $$($$(1)_C_OBJS)
+	$(3) -o $$@ $$(filter %.o, $$^) $(10)
+
 # Output Directories ###########################################################
 
 $$($$$(1)_DIRS):
@@ -125,4 +139,6 @@ $(eval $(call BUILD_TEMPLATE, $(TARGET_NAME), \
                               $(TARGET_LD), \
                               $(TARGET_ARFLAGS), \
                               $(TARGET_AR), \
-                              $(TARGET_VARIANT_SRCS)))
+                              $(TARGET_VARIANT_SRCS), \
+                              $(TARGET_BUILD_BIN), \
+                              $(TARGET_BIN_LDFLAGS)))
