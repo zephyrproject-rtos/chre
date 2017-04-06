6102275f42fd3ece47d6d9ca8c244e3f60006b53
Meng-hsuan Chung <menghsuan@google.com>
2017-04-06 13:51:39 -0700

    Implements the ashSaveCalibrationParams and ashLoadCalibrationParams ASH API
    Test: use TimerWorld to write to it and read it back.
    
    Change-Id: I6de9ed1fbaf9497225d827a3276650bc3801dc8e

--- ash/platform/linux/ash.cc
+++ ash/platform/linux/ash.cc
@@ -29,3 +29,15 @@ bool ashSetCalibration(uint8_t sensorType, const struct ashCalInfo *calInfo) {
   // TODO: Implement this.
   return false;
 }
+
+bool ashSaveCalibrationParams(uint8_t sensorType,
+                              const struct ashCalParams *calParams) {
+  // TODO: Implement this.
+  return false;
+}
+
+bool ashLoadCalibrationParams(uint8_t sensorType,
+                              struct ashCalParams *calParams) {
+  // TODO: Implement this.
+  return false;
+}
