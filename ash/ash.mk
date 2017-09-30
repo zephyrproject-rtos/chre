6db8da05adfb8006ffbc52953e42cce13a62ce4b
Meng-hsuan Chung <menghsuan@google.com>
2017-09-29 19:52:38 -0700

    Adds support to CHRE for google_hexagonv65_slpi-see
    Test: compile only
    Change-Id: I8d2beb7812c732ee475a38b057759e6bd110cace

--- ash/ash.mk
+++ ash/ash.mk
@@ -11,6 +11,10 @@ COMMON_CFLAGS += -Iash/include
 
 SLPI_SMGR_SRCS += ash/platform/slpi/smgr/ash.cc
 
+# SLPI/SEE-specific Source Files ###############################################
+
+SLPI_SEE_SRCS += ash/platform/slpi/see/ash.cc
+
 # x86-specific Source Files ####################################################
 
 X86_SRCS += ash/platform/linux/ash.cc
