8ad66ca1c67807c95143facfa9803c511a545965
Brian Duddie <bduddie@google.com>
2017-11-06 15:13:38 -0800

    Avoid deadlock with SMR
    SMR holds the client mutex when invoking response callbacks, and also
    acquires it when issuing requests. CHRE previously acquired a mutex
    prior to issuing a request, and also when handling a response. This
    interaction could lead to deadlock if a request times out.
    
    Address this by (1) increasing the default SMR request timeout from 1 to
    2 seconds, and (2) refactoring chre::SmrHelper to not hold a mutex when
    issuing a request. As part of (2), also update SmrHelper to free memory
    associated with a timed out request if the callback is received after
    the timeout.
    
    Bug: 68862548
    Test: force timeout by setting timer very low, monitor logs to ensure
    expected behavior arises, also use SensorWorld to check basic sensor
    sanity
    
    Change-Id: Ib7c6743dc2c0b152fe4bc782c2a73eedfdfa2f24

--- platform/slpi/smgr/smr_helper.cc
+++ platform/slpi/smgr/smr_helper.cc
@@ -22,6 +22,7 @@
 #include "chre/platform/log.h"
 #include "chre/platform/slpi/power_control_util.h"
 #include "chre/util/lock_guard.h"
+#include "chre/util/memory.h"
 
 namespace chre {
 
@@ -30,15 +31,17 @@ smr_err SmrHelper::releaseSync(smr_client_hndl clientHandle,
   // smr_client_release is synchronous for SMR services in the current
   // implementation, so we can't hold the lock while calling it otherwise we'll
   // deadlock in the callback.
-  {
-    LockGuard<Mutex> lock(mMutex);
-    CHRE_ASSERT(!mWaiting);
-    mWaiting = true;
-  }
+  prepareForWait();
 
   smr_err result = smr_client_release(
       clientHandle, SmrHelper::smrReleaseCb, this);
-  if (result == SMR_NO_ERR) {
+  if (result != SMR_NO_ERR) {
+    LOGE("SMR client release failed: %d", result);
+
+    // Don't need to re-acquire the mutex for this, as we are assured that the
+    // callback won't be invoked in the error case
+    mWaiting = false;
+  } else {
     LockGuard<Mutex> lock(mMutex);
     bool waitSuccess = true;
     while (mWaiting && waitSuccess) {
@@ -59,16 +62,18 @@ smr_err SmrHelper::waitForService(qmi_idl_service_object_type serviceObj,
                                   Microseconds timeout) {
   // smr_client_check_ext is synchronous if the service already exists,
   // so don't hold the lock while calling to prevent deadlock in the callback.
-  {
-    LockGuard<Mutex> lock(mMutex);
-    CHRE_ASSERT(!mWaiting);
-    mWaiting = true;
-  }
+  prepareForWait();
 
   smr_err result = smr_client_check_ext(serviceObj, SMR_CLIENT_INSTANCE_ANY,
                                         timeout.getMicroseconds(),
                                         SmrHelper::smrWaitForServiceCb, this);
-  if (result == SMR_NO_ERR) {
+  if (result != SMR_NO_ERR) {
+    LOGE("Failed to wait for service: %d", result);
+
+    // Don't need to re-acquire the mutex for this, as we are assured that the
+    // callback won't be invoked in the error case
+    mWaiting = false;
+  } else {
     LockGuard<Mutex> lock(mMutex);
     while (mWaiting) {
       mCond.wait(mMutex);
@@ -89,25 +94,41 @@ bool SmrHelper::sendReqSyncUntyped(
     void *req_c_struct, unsigned int req_c_struct_len,
     void *resp_c_struct, unsigned int resp_c_struct_len,
     Nanoseconds timeout, smr_err *result) {
-  LockGuard<Mutex> lock(mMutex);
-  CHRE_ASSERT(!mWaiting);
+  // Set to false only in the event of timeout
   bool waitSuccess = true;
 
   // Force big image since smr_client_send_req is not supported in micro-image
   slpiForceBigImage();
+
+  UniquePtr<SmrTransaction> txn = MakeUnique<SmrTransaction>();
+  if (txn.isNull()) {
+    FATAL_ERROR_OOM();
+  }
+  txn->parent = this;
+  txn->reqBuf = req_c_struct;
+  txn->rspBuf = resp_c_struct;
+  txn->transactionId = mCurrentTransactionId;
+
+  // Avoid holding the mutex while calling smr_client_send_req() - deadlock
+  // could arise if we did, due to acquisition order for SMR's client_mutex in
+  // the response flow
+  prepareForWait();
 
   // Note that null txn_handle means we can't abandon the transaction, but it's
   // only supported for QMI (non-SMR) services, and we don't expect that anyway.
   // SMR itself does not support canceling transactions made to SMR services.
   *result = smr_client_send_req(
       client_handle, msg_id, req_c_struct, req_c_struct_len, resp_c_struct,
-      resp_c_struct_len, SmrHelper::smrRespCb, this, nullptr /* txn_handle */);
+      resp_c_struct_len, SmrHelper::smrRespCb, txn.get(),
+      nullptr /* txn_handle */);
   if (*result != SMR_NO_ERR) {
     LOGE("Failed to send request (msg_id 0x%02x): %d", msg_id, *result);
-  } else {
-    mWaiting = true;
-    mPendingRespBuf = resp_c_struct;
 
+    // Don't need to re-acquire the mutex for this, as we are assured that the
+    // callback won't be invoked in the error case
+    mWaiting = false;
+  } else {
+    LockGuard<Mutex> lock(mMutex);
     while (mWaiting && waitSuccess) {
       waitSuccess = mCond.wait_for(mMutex, timeout);
     }
@@ -118,9 +139,15 @@ bool SmrHelper::sendReqSyncUntyped(
       LOGE("SMR request for msg_id 0x%02x timed out after %" PRIu64 " ms",
            msg_id, Milliseconds(timeout).getMilliseconds());
       *result = SMR_TIMEOUT_ERR;
+
+      // If the callback comes later, we'll recognize the mismatched transaction
+      // ID and drop the response. Note that we don't increment this in the
+      // successful case, as it's not necessary in that case, and this gives us
+      // a way to track the number of timeouts we've hit.
+      mCurrentTransactionId++;
       mWaiting = false;
+      txn.release();
     }
-    mPendingRespBuf = nullptr;
   }
 
   return waitSuccess;
@@ -128,14 +155,18 @@ bool SmrHelper::sendReqSyncUntyped(
 
 void SmrHelper::handleResp(smr_client_hndl client_handle, unsigned int msg_id,
                            void *resp_c_struct, unsigned int resp_c_struct_len,
-                           smr_err transp_err) {
+                           smr_err transp_err, SmrTransaction *txn) {
   LockGuard<Mutex> lock(mMutex);
 
-  if (!mWaiting) {
-    LOGE("Got SMR response when none pending!");
-  } else if (mPendingRespBuf != resp_c_struct) {
-    LOGE("Got SMR response with unexpected buffer, msg_id 0x%02x: %p vs. %p",
-         msg_id, mPendingRespBuf, resp_c_struct);
+  if (!mWaiting || txn->transactionId != mCurrentTransactionId) {
+    LOGE("Got expired SMR response (my ID %" PRIu32 " vs current %" PRIu32 ")",
+         txn->transactionId, mCurrentTransactionId);
+
+    // If this happens, it means the requestor timed out, so it's depending on
+    // us to release the memory
+    memoryFree(txn->reqBuf);
+    memoryFree(txn->rspBuf);
+    memoryFree(txn);
   } else {
     // SMR will handle copying the response into the buffer passed in to
     // smr_client_send_req(), so we just need to unblock the waiting thread
@@ -143,6 +174,12 @@ void SmrHelper::handleResp(smr_client_hndl client_handle, unsigned int msg_id,
     mWaiting = false;
     mCond.notify_one();
   }
+}
+
+void SmrHelper::prepareForWait() {
+  LockGuard<Mutex> lock(mMutex);
+  CHRE_ASSERT(!mWaiting);
+  mWaiting = true;
 }
 
 void SmrHelper::smrReleaseCb(void *release_cb_data) {
@@ -155,15 +192,15 @@ void SmrHelper::smrReleaseCb(void *release_cb_data) {
 void SmrHelper::smrRespCb(smr_client_hndl client_handle, unsigned int msg_id,
                           void *resp_c_struct, unsigned int resp_c_struct_len,
                           void *resp_cb_data, smr_err transp_err) {
-  SmrHelper *obj = static_cast<SmrHelper *>(resp_cb_data);
-  obj->handleResp(client_handle, msg_id, resp_c_struct, resp_c_struct_len,
-                  transp_err);
+  auto *txn = static_cast<SmrTransaction *>(resp_cb_data);
+  txn->parent->handleResp(
+      client_handle, msg_id, resp_c_struct, resp_c_struct_len, transp_err, txn);
 }
 
-void SmrHelper::smrWaitForServiceCb(qmi_idl_service_object_type /* service_obj */,
-                                    qmi_service_instance /* instance_id */,
-                                    bool timeout_expired,
-                                    void *wait_for_service_cb_data) {
+void SmrHelper::smrWaitForServiceCb(
+    qmi_idl_service_object_type /* service_obj */,
+    qmi_service_instance /* instance_id */, bool timeout_expired,
+    void *wait_for_service_cb_data) {
   SmrHelper *obj = static_cast<SmrHelper *>(wait_for_service_cb_data);
   LockGuard<Mutex> lock(obj->mMutex);
   obj->mServiceTimedOut = timeout_expired;
