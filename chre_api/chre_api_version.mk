920f4503e3d486d99e416d08787b23dfd40b7e09
Andrew Rossignol <aarossig@google.com>
2018-02-01 16:40:17 -0800

    Ensure out/ exists for CHRE API version tool
    Test: ./run_sim.sh
    Change-Id: I19beca14ed2bd67311604c175293faca83b007e9

--- chre_api/chre_api_version.mk
+++ chre_api/chre_api_version.mk
@@ -10,6 +10,7 @@ PRINT_CURRENT_CHRE_API_VERSION_BIN = $(OUT)/print_current_chre_api_version
 CURRENT_CHRE_API_VERSION_MK = $(OUT)/current_chre_api_version.mk
 
 $(PRINT_CURRENT_CHRE_API_VERSION_BIN): $(PRINT_CURRENT_CHRE_API_VERSION_SRCS)
+	mkdir -p $(OUT)
 	$(CHRE_HOST_CC) -I$(CHRE_PREFIX)/chre_api/include/chre_api $^ -o $@
 
 $(CURRENT_CHRE_API_VERSION_MK): $(PRINT_CURRENT_CHRE_API_VERSION_BIN)
