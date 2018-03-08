f3f167baf5e56997359cb3c63412fe24708725fe
Andrew Rossignol <aarossig@google.com>
2018-03-08 14:36:38 -0800

    Remove hardcoded branchname from bundle_chre.sh script
    - This ensures that the bundled branch is correct
    
    Bug: 74344985
    Test: ./bundle_chre.sh
    Change-Id: I7e54cf9638bdbb8d10d2af939923c314895381b0

--- bundle_chre.sh
+++ bundle_chre.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 #
 # Generates a bundle of this project to share with partners. It produces a file
 # with the name chre-$HEAD_HASH.bundle where $HEAD_HASH is the current ToT
@@ -7,6 +7,10 @@
 # Usage:
 #     ./bundle_chre.sh
 
-BRANCH=goog/master
+BRANCH=`git rev-parse --abbrev-ref --symbolic-full-name @{u}`
 HEAD_HASH=`git describe --always --long $BRANCH`
-git bundle create chre-$HEAD_HASH.bundle $BRANCH
+FILENAME=chre-$HEAD_HASH.bundle
+git bundle create $FILENAME $BRANCH
+
+echo
+echo "Created bundle for $BRANCH as $FILENAME"
