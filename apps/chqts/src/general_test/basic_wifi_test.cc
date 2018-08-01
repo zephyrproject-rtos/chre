dbbc13fe636c653b0d5b1551ac484ade5acc5397
Jack Sun <jacksun@google.com>
2018-08-01 15:21:41 -0700

    Makes BasicWifiTest event-driven
    Modifies BasicWifiTest to sequentially execute tests after
    receiving expected events. Also adds validation for chreAsyncEvent.
    
    Bug: 111657325
    Test: testContextHubSimpleGeneralNanoAppTests passed on walleye.
    Change-Id: I07eadd52ce2957fecec52aafd5cfb1953d301e57

--- apps/chqts/src/general_test/basic_wifi_test.cc
+++ apps/chqts/src/general_test/basic_wifi_test.cc
@@ -21,31 +21,60 @@
 
 /*
  * Test to check expected functionality of the CHRE WiFi APIs.
- * TODO: Currently the test only exists to verify that expected APIs are
- *       implemented and doesn't fail. Make the test more comprehensive by
- *       validating callback results, etc.
+ *
+ * 1. If scan monitor is not supported, skip to 5;
+ *    otherwise enables scan monitor.
+ * 2. Checks async result of enabling scan monitor.
+ * 3. Disables scan monitor.
+ * 4. Checks async result of disabling scan monitor.
+ * 5. If on demand WiFi scan is not supported, skip to end;
+ *    otherwise sends default scan request.
+ * 6. Checks the result of on demand WiFi scan.
  */
 namespace general_test {
 
 namespace {
 
-void testConfigureScanMonitorAsync() {
-  if (!chreWifiConfigureScanMonitorAsync(
-          true /* enable */, nullptr /* cookie */)) {
-    nanoapp_testing::sendFatalFailureToHost(
-        "Failed to request to enable scan monitor.");
-  }
-  if (!chreWifiConfigureScanMonitorAsync(
-          false /* enable */, nullptr /* cookie */)) {
-    nanoapp_testing::sendFatalFailureToHost(
-        "Failed to request to disable scan monitor.");
+//! A dummy cookie to pass into the enable
+//! configure scan monitoring async request.
+constexpr uint32_t kEnableScanMonitoringCookie = 0x1337;
+
+//! A dummy cookie to pass into the disable
+//! configure scan monitoring async request.
+constexpr uint32_t kDisableScanMonitoringCookie = 0x1338;
+
+//! A dummy cookie to pass into request scan async.
+constexpr uint32_t kOnDemandScanCookie = 0xcafe;
+
+/**
+ * Calls API testConfigureScanMonitorAsync. Sends fatal failure to host
+ * if API call fails.
+ *
+ * @param enable Set to true to enable monitoring scan results,
+ *        false to disable.
+ * @param cookie An opaque value that will be included in the chreAsyncResult
+ *        sent in relation to this request.
+ */
+void testConfigureScanMonitorAsync(bool enable, const void * cookie) {
+  if (!chreWifiConfigureScanMonitorAsync(enable, cookie)) {
+    if (enable) {
+      nanoapp_testing::sendFatalFailureToHost(
+          "Failed to request to enable scan monitor.");
+    } else {
+      nanoapp_testing::sendFatalFailureToHost(
+          "Failed to request to disable scan monitor.");
+    }
   }
 }
 
+/**
+ * Calls API chreWifiRequestScanAsyncDefault. Sends fatal failure to host
+ * if API call fails.
+ */
 void testRequestScanAsync() {
-  if (!chreWifiRequestScanAsyncDefault(nullptr /* cookie */)) {
+  if (!chreWifiRequestScanAsyncDefault(&kOnDemandScanCookie)) {
     nanoapp_testing::sendFatalFailureToHost(
-        "Failed to request for on-demand wifi scan.");
+        "Failed to request for on-demand WiFi scan.");
   }
 }
 
@@ -61,27 +90,101 @@ void BasicWifiTest::setUp(
     nanoapp_testing::sendFatalFailureToHost(
         "Expected 0 byte message, got more bytes:", &messageSize);
   } else {
-    uint32_t capabilities = chreWifiGetCapabilities();
-
-    if (capabilities & CHRE_WIFI_CAPABILITIES_SCAN_MONITORING) {
-      testConfigureScanMonitorAsync();
-    }
-    if (capabilities & CHRE_WIFI_CAPABILITIES_ON_DEMAND_SCAN) {
-      testRequestScanAsync();
-    }
-    // TODO: Add test for chreWifiRequestRangingAsync
-
-    nanoapp_testing::sendSuccessToHost();
+    mWifiCapabilities = chreWifiGetCapabilities();
+    startScanMonitorTestStage();
   }
 }
 
 void BasicWifiTest::handleEvent(uint32_t /* senderInstanceId */,
                                 uint16_t eventType,
-                                const void * /* eventData */) {
-  if (eventType != CHRE_EVENT_WIFI_ASYNC_RESULT
-      && eventType != CHRE_EVENT_WIFI_SCAN_RESULT) {
-    unexpectedEvent(eventType);
+                                const void *eventData) {
+  if (eventData == nullptr) {
+    nanoapp_testing::sendFatalFailureToHost(
+        "Received null eventData");
   }
+  switch (eventType) {
+    case CHRE_EVENT_WIFI_ASYNC_RESULT:
+      handleChreWifiAsyncEvent(eventData);
+      break;
+    case CHRE_EVENT_WIFI_SCAN_RESULT:
+      if (isActiveWifiScanType(eventData)) {
+        // TODO (jacksun) remove the follwing line,
+        // and add validating wifi scan results.
+        mTestSuccessMarker.markStageAndSuccessOnFinish(
+            BASIC_WIFI_TEST_STAGE_SCAN_ASYNC);
+      }
+      break;
+    default:
+      unexpectedEvent(eventType);
+      break;
+  }
+}
+
+void BasicWifiTest::handleChreWifiAsyncEvent(const void *eventData) {
+  if (!mCurrentWifiRequest.has_value()) {
+    nanoapp_testing::sendFailureToHost("Unexpected async result");
+  }
+  const auto *result =
+    static_cast<const chreAsyncResult *>(eventData);
+  validateChreAsyncResult(result, mCurrentWifiRequest.value());
+
+  if (result->requestType == CHRE_WIFI_REQUEST_TYPE_CONFIGURE_SCAN_MONITOR) {
+    if (mCurrentWifiRequest->cookie == &kDisableScanMonitoringCookie) {
+      mTestSuccessMarker.markStageAndSuccessOnFinish(
+          BASIC_WIFI_TEST_STAGE_SCAN_MONITOR);
+      startScanAsyncTestStage();
+    } else {
+      testConfigureScanMonitorAsync(false /* enable */,
+                                    &kDisableScanMonitoringCookie);
+      resetCurrentWifiRequest(&kDisableScanMonitoringCookie,
+                              CHRE_WIFI_REQUEST_TYPE_CONFIGURE_SCAN_MONITOR,
+                              CHRE_ASYNC_RESULT_TIMEOUT_NS);
+    }
+  }
+}
+
+bool BasicWifiTest::isActiveWifiScanType(const void *eventData) {
+  const auto *result =
+      static_cast<const chreWifiScanEvent *>(eventData);
+  return (result->scanType == CHRE_WIFI_SCAN_TYPE_ACTIVE);
+}
+
+void BasicWifiTest::startScanMonitorTestStage() {
+  if (mWifiCapabilities & CHRE_WIFI_CAPABILITIES_SCAN_MONITORING) {
+    testConfigureScanMonitorAsync(true /* enable */,
+                                  &kEnableScanMonitoringCookie);
+    resetCurrentWifiRequest(&kEnableScanMonitoringCookie,
+                            CHRE_WIFI_REQUEST_TYPE_CONFIGURE_SCAN_MONITOR,
+                            CHRE_ASYNC_RESULT_TIMEOUT_NS);
+  } else {
+    mTestSuccessMarker.markStageAndSuccessOnFinish(
+        BASIC_WIFI_TEST_STAGE_SCAN_MONITOR);
+    startScanAsyncTestStage();
+  }
+}
+
+void BasicWifiTest::startScanAsyncTestStage() {
+  if (mWifiCapabilities & CHRE_WIFI_CAPABILITIES_ON_DEMAND_SCAN) {
+    testRequestScanAsync();
+    resetCurrentWifiRequest(&kOnDemandScanCookie,
+                            CHRE_WIFI_REQUEST_TYPE_REQUEST_SCAN,
+                            CHRE_WIFI_SCAN_RESULT_TIMEOUT_NS);
+  } else {
+    mTestSuccessMarker.markStageAndSuccessOnFinish(
+        BASIC_WIFI_TEST_STAGE_SCAN_ASYNC);
+  }
+}
+
+void BasicWifiTest::resetCurrentWifiRequest(const void *cookie,
+                                            uint8_t requstType,
+                                            uint64_t timeoutNs) {
+  chreAsyncRequest request = {
+    .cookie = cookie,
+    .requestType = requstType,
+    .requestTimeNs = chreGetTime(),
+    .timeoutNs = timeoutNs
+  };
+  mCurrentWifiRequest = request;
 }
 
 } // namespace general_test
--- apps/chqts/src/general_test/basic_wifi_test.h
+++ apps/chqts/src/general_test/basic_wifi_test.h
@@ -21,17 +21,100 @@
 #include <cstdint>
 
 #include <chre.h>
+#include <shared/test_success_marker.h>
+
+#include "chre/util/optional.h"
 
 namespace general_test {
 
+/**
+ * A class which tests chre WiFi APIs, including:
+ * chreWifiConfigureScanMonitorAsync
+ * chreWifiRequestScanAsync
+ * chreWifiRequestRangingAsync.
+ *
+ * Sends requests to those APIs and validates subsequent event data.
+ * Sends success to host if all data is valid, otherwise sends fatal failure.
+ */
 class BasicWifiTest : public Test {
  public:
   BasicWifiTest();
-
  protected:
+  /**
+   * Handles WiFi events, including:
+   * CHRE_EVENT_WIFI_ASYNC_RESULT
+   * CHRE_EVENT_WIFI_SCAN_RESULT
+   * CHRE_EVENT_WIFI_RANGING_RESULT
+   *
+   * @param senderInstanceId instance id from which the event is sent.
+   * @param eventType one of the above events.
+   * @param eventData a pointer to the details of a WiFi event.
+   */
   void handleEvent(uint32_t senderInstanceId, uint16_t eventType,
                    const void *eventData) override;
+
+  /**
+   * Calls chreWifiConfigureScanMonitorAsync with enable = true
+   * if WiFi has scan monitor capability, otherwise calls
+   * chreWifiRequestScanAsync if WiFi has on demand scan capability.
+   *
+   * @param messageSize the size of received message.
+   * @param message a pointer to the received message.
+   */
   void setUp(uint32_t messageSize, const void *message) override;
+ private:
+  /**
+   * Validates chre WiFi async events.
+   * If validation result is true, makes subsequent requests:
+   * chreWifiConfigureScanMonitorAsync with enable = false
+   * chreWifiRequestScanAsyncDefault
+   *
+   * @param eventData received WiFi async result data.
+   */
+  void handleChreWifiAsyncEvent(const void *eventData);
+
+  /**
+   * @param eventData received WiFi scan event data.
+   * @return true if scanType is CHRE_WIFI_SCAN_TYPE_ACTIVE, false otherwise.
+   */
+  bool isActiveWifiScanType(const void *eventData);
+
+  /**
+   * Makes an API call, if corresponding WiFi ability exists;
+   * otherwise procceeds to next test stage.
+   */
+  void startScanMonitorTestStage();
+  void startScanAsyncTestStage();
+
+  /**
+   * This method must be called after making an async request to CHRE.
+   *
+   * @param cookie pointer to request cookie.
+   * @param requestType a type of request.
+   * @param timeoutNs expected maximum elapse to receive chre WiFi result.
+   */
+  void resetCurrentWifiRequest(const void *cookie,
+                               uint8_t requstType,
+                               uint64_t timeoutNs);
+
+  /**
+   * Basic WiFi test stages and total number of stages.
+   */
+  enum BasicWifiTestStage {
+    BASIC_WIFI_TEST_STAGE_SCAN_MONITOR = 0,
+    BASIC_WIFI_TEST_STAGE_SCAN_ASYNC,
+    BASIC_WIFI_TEST_STAGE_COUNT,
+  };
+
+  //! WiFi capabilities, used to make corresponding API calls.
+  uint32_t mWifiCapabilities;
+
+  //! TestSuccessMarker object to mark success of a stage.
+  nanoapp_testing::TestSuccessMarker mTestSuccessMarker =
+      nanoapp_testing::TestSuccessMarker(BASIC_WIFI_TEST_STAGE_COUNT);
+
+  //! Used to indicate if a chreAsyncResult is being expected.
+  chre::Optional<chreAsyncRequest> mCurrentWifiRequest;
 };
 
 } // namespace general_test
