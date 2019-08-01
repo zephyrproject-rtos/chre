e4265a1250f02c63c2df84b2189776edd14d7abd
Stan Rokita <srok@google.com>
2019-08-01 12:18:53 -0700

    Fix compile error in log host wakeups
    Test: It compiles now.
    
    Change-Id: I94c4ff16104da3da5fc06a7ce77c01a36015ae47

--- platform/slpi/smgr/power_control_manager.cc
+++ platform/slpi/smgr/power_control_manager.cc
@@ -22,7 +22,7 @@
 
 namespace chre {
 
-PowerControlManagerBase::PowerControlManagerBase() : mIsHostAwake(true) {
+PowerControlManagerBase::PowerControlManagerBase() : mHostIsAwake(true) {
 #ifdef CHRE_SLPI_UIMG_ENABLED
   char kClientName[] = "CHRE";
   sns_pm_err_code_e result = sns_pm_client_init(
