508a8c22f4f75c494c2550d1643a9a9a0240e791
Anthony Stange <stange@google.com>
2019-10-14 10:35:25 -0400

    Fix implicit conversion warning for simulator
    Since messageSize is passed in as a size_t by
    sendMessageToNanoappFromHost, craftNanoappMessageFromHost should also
    take in a size_t to avoid an implicit conversion warning.
    
    Test: ./run_sim.sh
    Change-Id: Id053b7a09fdbbf3a3b9b7fa8917e17b5b4415c2b

--- core/include/chre/core/host_comms_manager.h
+++ core/include/chre/core/host_comms_manager.h
@@ -198,7 +198,7 @@ class HostCommsManager : public NonCopyable {
                                                uint16_t hostEndpoint,
                                                uint32_t messageType,
                                                const void *messageData,
-                                               uint32_t messageSize);
+                                               size_t messageSize);
 
   /**
    * Posts a crafted event, craftedMessage, to a nanoapp for processing, and
