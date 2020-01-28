f77d502699a7a978fcd2e75595dc1c4715e4e8ef
Stan Rokita <srok@google.com>
2020-01-27 17:02:32 -0800

    Implement nanoapp handle start sensor messages
    Bug: 146052784
    Test: Build succesffully
    Change-Id: I1aba35e39ab5377070e87cd7c6d4e002c02df000

--- apps/test/common/chre_cross_validator/src/chre_cross_validator.cc
+++ apps/test/common/chre_cross_validator/src/chre_cross_validator.cc
@@ -21,17 +21,105 @@
  * given in start message.
  */
 
+#include <cinttypes>
+
 #include <chre.h>
+#include <pb_decode.h>
 #include <pb_encode.h>
 
 #include "chre/util/nanoapp/log.h"
 #include "chre/util/time.h"
 #include "chre_cross_validation.nanopb.h"
 
+#define LOG_TAG "ChreCrossValidator"
+
+/* TODO(b/148481242): Send all errors to host as well as just logging them as
+ * errors.
+ *
+ * TODO(b/146052784): Move start and handle data methods for each cross
+ * validation type (sensor[accel, gyro, ...], wifi, gps) to a manager class.
+ *
+ * TODO(b/146052784): Craete a helper function to get string version of
+ * sensorType for logging.
+ */
+
 namespace chre {
 
+namespace {
+
+void handleStartSensorMessage(
+    const chre_cross_validation_StartSensorCommand &startSensorCommand) {
+  uint8_t sensorType = startSensorCommand.sensorType;
+  uint64_t interval = startSensorCommand.samplingIntervalInNs;
+  uint64_t latency = startSensorCommand.samplingMaxLatencyInNs;
+  uint32_t handle;
+  if (!chreSensorFindDefault(sensorType, &handle)) {
+    LOGE("Could not find default sensor for sensorType %" PRIu8, sensorType);
+    // TODO(b/146052784): Test other sensor configure modes
+  } else if (!chreSensorConfigure(handle, CHRE_SENSOR_CONFIGURE_MODE_CONTINUOUS,
+                                  interval, latency)) {
+    LOGE("Error configuring sensor with sensorType %" PRIu8
+         ", interval %" PRIu64 "ns, and latency %" PRIu64 "ns",
+         sensorType, interval, latency);
+  } else {
+    LOGD("Sensor with sensor type %" PRIu8 " configured", sensorType);
+  }
+}
+
+void handleStartMessage(const chreMessageFromHostData *hostData) {
+  pb_istream_t istream = pb_istream_from_buffer(
+      static_cast<const pb_byte_t *>(hostData->message), hostData->messageSize);
+  chre_cross_validation_StartCommand startCommand =
+      chre_cross_validation_StartCommand_init_default;
+  if (!pb_decode(&istream, chre_cross_validation_StartCommand_fields,
+                 &startCommand)) {
+    LOGE("Could not decode start command");
+  } else {
+    switch (startCommand.which_command) {
+      case chre_cross_validation_StartCommand_startSensorCommand_tag:
+        handleStartSensorMessage(startCommand.command.startSensorCommand);
+        break;
+      default:
+        LOGE("Unknown start command type %" PRIu8, startCommand.which_command);
+    }
+  }
+}
+
+void handleMessageFromHost(uint32_t senderInstanceId,
+                           const chreMessageFromHostData *hostData) {
+  if (senderInstanceId != CHRE_INSTANCE_ID) {
+    LOGE("Incorrect sender instance id: %" PRIu32, senderInstanceId);
+  } else {
+    switch (hostData->messageType) {
+      case chre_cross_validation_MessageType_CHRE_CROSS_VALIDATION_START:
+        handleStartMessage(hostData);
+        break;
+      default:
+        LOGE("Unknown message type %" PRIu32 " for host message",
+             hostData->messageType);
+    }
+  }
+}
+
+}  // namespace
+
 extern "C" void nanoappHandleEvent(uint32_t senderInstanceId,
-                                   uint16_t eventType, const void *eventData) {}
+                                   uint16_t eventType, const void *eventData) {
+  switch (eventType) {
+    case CHRE_EVENT_MESSAGE_FROM_HOST:
+      handleMessageFromHost(
+          senderInstanceId,
+          static_cast<const chreMessageFromHostData *>(eventData));
+      break;
+    case CHRE_EVENT_SENSOR_ACCELEROMETER_DATA:
+      // TODO(b/146052784): Implement
+      break;
+    default:
+      LOGE("Got unknown event type from senderInstanceId %" PRIu32
+           " and with eventType %" PRIu16,
+           senderInstanceId, eventType);
+  }
+}
 
 extern "C" bool nanoappStart(void) {
   return true;
