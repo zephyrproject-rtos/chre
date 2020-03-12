0b03b5faa697ac9d6e185586053886acebc3dc2f
Ahmad Rahmati <rahmati@google.com>
2020-03-11 22:08:29 -0700

    Structure App Layer for predefined services
    Refactor the App Layer to separately route Rx datagrams to predefined
    services
    
    Bug: 146020579
    Test: ran "atest --host chre_chpp_linux_tests -c"
    Change-Id: Idbda5c6cb6584e782d96df558f91e7dfcaffffab

--- chpp/include/chpp/app.h
+++ chpp/include/chpp/app.h
@@ -44,11 +44,62 @@ extern "C" {
  ***********************************************/
 
 /**
- * Loopback functionality.
+ * Handle Numbers in ChppAppHeader
  */
-#define CHPP_DATAGRAM_HANDLE_LOOPBACK 0x01
-#define CHPP_DATAGRAM_TYPE_CLIENT_REQUEST 0x00
-#define CHPP_DATAGRAM_TYPE_SERVER_RESPONSE 0x01
+enum ChppHandleNumber {
+  // Handleless communication
+  CHPP_HANDLE_NONE = 0x00,
+
+  // Loopback Service
+  CHPP_HANDLE_LOOPBACK = 0x01,
+
+  // Discovery Service
+  CHPP_HANDLE_DISCOVERY = 0x0F,
+
+  // Negotiated Services (starting from this offset)
+  CHPP_HANDLE_NEGOTIATED_RANGE_START = 0x10,
+};
+
+/**
+ * Message Types as used in ChppAppHeader
+ */
+enum ChppMessageType {
+  // Request from client. Needs response from server.
+  CHPP_MESSAGE_TYPE_CLIENT_REQUEST = 0,
+
+  // Response from server (with the same Command and Transaction ID as the
+  // client request).
+  CHPP_MESSAGE_TYPE_SERVER_RESPONSE = 1,
+
+  //  Notification from client. Server shall not respond.
+  CHPP_MESSAGE_TYPE_CLIENT_NOTIFICATION = 2,
+
+  // Notification from server. Client shall not respond.
+  CHPP_MESSAGE_TYPE_SERVER_NOTIFICATION = 4,
+};
+
+/**
+ * CHPP Application Layer header
+ */
+CHPP_PACKED_START
+struct ChppAppHeader {
+  // Service Handle
+  uint8_t handle;
+
+  // Message Type
+  uint8_t type;
+
+  // Transaction ID
+  uint8_t transaction;
+
+  // Reserved
+  uint8_t reserved;
+
+  // Command
+  uint16_t command;
+
+} CHPP_PACKED_ATTR;
+CHPP_PACKED_END
 
 struct ChppAppState {
   struct ChppTransportState *transportContext;  // Pointing to transport context
@@ -71,9 +122,9 @@ void chppAppInit(struct ChppAppState *appContext,
                  struct ChppTransportState *transportContext);
 
 /*
- * Processes an Rx Datagram from the transport layer. It is up to the app layer
- * to call chppAppProcessDoneCb() ASAP when it is done with buf (so it is
- * properly freed, etc.)
+ * Processes an Rx Ddtagram from the transport layer. This function calls
+ * chppAppProcessDoneCb() ASAP when it is done with buf (so it is properly freed
+ * by the Transport Layer, etc.).
  *
  * @param context Maintains status for each app layer instance.
  * @param buf Input data. Cannot be null.
