91b5c1edc63bcc34a4fb56a6ac180cd95a084170
Brian Duddie <bduddie@google.com>
2020-05-21 22:52:45 -0700

    Ensure offset is zero if length is zero for VLAs
    Makes for a cleaner encoding - offset set to 0 is inherently an invalid
    value, so helps to catch potential issues down the line.
    
    Test: tested in follow-up CL
    Change-Id: I3f3fe149c50ebc3312626b651f39c5cafd1be23b

--- chpp/api_parser/chre_api_to_chpp.py
+++ chpp/api_parser/chre_api_to_chpp.py
@@ -371,12 +371,16 @@ class CodeGenerator:
         chpp_type = self._get_member_type(member_info, True)
         out.append("\n    {} *{} = ({} *) &payload[*vlaOffset];\n".format(
             chpp_type, variable_name, chpp_type))
-        out.append("    out->{}.offset = *vlaOffset;\n".format(member_info['name']))
         out.append("    out->{}.length = in->{} * {};\n".format(
             member_info['name'], annotation['length_field'],
             self._get_chpp_member_sizeof_call(member_info)))
-        out.append("    *vlaOffset += out->{}.length;\n".format(member_info['name']))
-        out.append("    CHPP_ASSERT(*vlaOffset <= payloadSize);\n"
+        out.append("    if (out->{}.length > 0) {{\n".format(member_info['name']))
+        out.append("        out->{}.offset = *vlaOffset;\n".format(member_info['name']))
+        out.append("        *vlaOffset += out->{}.length;\n".format(member_info['name']))
+        out.append("    } else {\n")
+        out.append("        out->{}.offset = 0;\n".format(member_info['name']))
+        out.append("    }\n"
+                   "    CHPP_ASSERT(*vlaOffset <= payloadSize);\n"
                    "    UNUSED_VAR(payloadSize);\n\n")
 
         out.append("    for (size_t i = 0; i < in->{}; i++) {{\n".format(
