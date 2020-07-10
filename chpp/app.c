5bd9529689c8d1c807d99be53e46a55a1db3ab02
Arthur Ishiguro <arthuri@google.com>
2020-07-10 14:37:18 -0700

    Use snprintf in CHPP for safety
    Bug: 160899751
    Test: chre_chpp_linux_tests
    Change-Id: I4b61e1bd0004e84526cd7f17a5c9527c5c94e4cb

--- chpp/app.c
+++ chpp/app.c
@@ -16,6 +16,8 @@
 
 #include "chpp/app.h"
 
+#include <stdio.h>
+
 #include "chpp/clients.h"
 #include "chpp/pal_api.h"
 #include "chpp/services.h"
@@ -535,8 +537,8 @@ void chppProcessRxDatagram(struct ChppAppState *context, uint8_t *buf,
 
 void chppUuidToStr(const uint8_t uuid[CHPP_SERVICE_UUID_LEN],
                    char strOut[CHPP_SERVICE_UUID_STRING_LEN]) {
-  sprintf(
-      strOut,
+  snprintf(
+      strOut, CHPP_SERVICE_UUID_STRING_LEN,
       "%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",
       uuid[0], uuid[1], uuid[2], uuid[3], uuid[4], uuid[5], uuid[6], uuid[7],
       uuid[8], uuid[9], uuid[10], uuid[11], uuid[12], uuid[13], uuid[14],
