03e7dd6a3349442fac52908e786a8109f816c27c
karthik bharadwaj <karthikmb@google.com>
2021-03-24 12:09:21 -0700

    Rename the microphone access user setting
    This CL renames the microphone access user setting, setting up for a
    refactor in the interpretation of this user setting in CHRE.
    
    Bug: 183416485
    Test: Compile - This CL is to avoid a CHRE build breakage, the actual
    logic for the refactor and associated tests will be in a separate CL.
    
    Change-Id: I73014ba949903a71c6cd5e98e9b25d9e734d5a79

--- platform/shared/host_protocol_chre.cc
+++ platform/shared/host_protocol_chre.cc
@@ -274,8 +274,7 @@ bool HostProtocolChre::getSettingFromFbs(fbs::Setting setting,
       *chreSetting = Setting::AIRPLANE_MODE;
       break;
     case fbs::Setting::MICROPHONE:
-      // TODO(b/183026207): Change the setting's enum name
-      *chreSetting = Setting::GLOBAL_MIC_DISABLE;
+      *chreSetting = Setting::MICROPHONE;
       break;
     default:
       LOGE("Unknown setting %" PRIu8, static_cast<uint8_t>(setting));
